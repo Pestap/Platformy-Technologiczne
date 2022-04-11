311e01f0f2ef69a342e9a3b303d7bc19b48015f2
Pestap <90579181+Pestap@users.noreply.github.com>
2022-04-11 21:16:42 +0200

    Zrobione zadanie zrefaktoryzowane

--- Lab/Lab6/src/main/java/Main.java
+++ Lab/Lab6/src/main/java/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,7 +19,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        //====================================================================
+        //strumien sekwencyjnie
+        //====================================================================
         long time = System.currentTimeMillis();
 
         List<Path> files = null;
@@ -48,18 +51,7 @@ public class Main {
             String name = pair.getLeft();
             BufferedImage img = pair.getRight();
 
-            for(int i =0; i < img.getWidth();i++ ){
-                for(int j =0; j < img.getHeight(); j++){
-                    int rgb = img.getRGB(i, j);
-                    Color color = new Color(rgb);
-                    int red = color.getRed();
-                    int blue = color.getBlue();
-                    int green = color.getGreen();
-                    Color outColor = new Color(red, blue, green);
-                    int outRgb = outColor.getRGB();
-                    img.setRGB(i,j,outRgb);
-                }
-            }
+            transformImage(img);
 
             File output = new File("Output\\"+name);
             try {
@@ -71,10 +63,74 @@ public class Main {
 
         long timeElapsed = System.currentTimeMillis() - time;
 
-        System.out.println("Czas trwania: " + timeElapsed/1000 + " sekund");
+        System.out.println("Czas trwania: " + timeElapsed/1000.0 + " sekund");
+
+        //====================================================================
+        //strumien równoległy
+        //====================================================================
 
 
+        time = System.currentTimeMillis();
 
+        List<Path> filesParallel = null;
+        Path sourceParallel = Path.of("Photos");
+        try (Stream<Path> stream = Files.list(source)) {
+            filesParallel = stream.collect(Collectors.toList());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //System.out.println(Runtime.getRuntime().availableProcessors());
+        ForkJoinPool own = new ForkJoinPool(6000);
+
+        Stream<Path> pathStreamParalel = filesParallel.stream().parallel();
+        try {
+            own.submit(() -> pathStreamParalel.map(path ->{
+                String name = path.getFileName().toString();
+                BufferedImage img = null;
+                try {
+                    img = ImageIO.read(path.toFile());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Pair<String, BufferedImage> pair = Pair.of(name,img);
+                return pair;
+            }).forEach(pair-> {
+                String name = pair.getLeft();
+                BufferedImage img = pair.getRight();
+
+                transformImage(img);
+
+                File output = new File("OutputP\\"+name);
+                try {
+                    ImageIO.write(img, "jpg", output);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            })).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        own.shutdown();
+
+        long timeElapsedParallel = System.currentTimeMillis() - time;
+        System.out.println("Czas trwania: " + timeElapsedParallel/1000.0 + " sekund");
+
+    }
+
+    public static void transformImage(BufferedImage img){
+        for(int i =0; i < img.getWidth();i++ ){
+            for(int j =0; j < img.getHeight(); j++){
+                int rgb = img.getRGB(i, j);
+                Color color = new Color(rgb);
+                int red = color.getRed();
+                int blue = color.getBlue();
+                int green = color.getGreen();
+                Color outColor = new Color(red, blue, green);
+                int outRgb = outColor.getRGB();
+                img.setRGB(i,j,outRgb);
+            }
+        }
     }
 
 }
