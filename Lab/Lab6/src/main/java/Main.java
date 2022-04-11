9b35be409a9457567234515a41cd833b22eb99d9
Pestap <90579181+Pestap@users.noreply.github.com>
2022-04-11 19:47:20 +0200

    To i tak nie działa

--- Lab/Lab6/src/main/java/Main.java
+++ Lab/Lab6/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import org.apache.commons.lang3.tuple.Pair;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageFilter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +16,11 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
+        long time = System.currentTimeMillis();
+
         List<Path> files = null;
         Path source = Path.of("Photos");
         try (Stream<Path> stream = Files.list(source)) {
@@ -37,43 +44,34 @@ public class Main {
             return pair;
         });
 
-        List<Pair<String, BufferedImage>> list = imageStream.toList();
-        Stream<Pair<String, BufferedImage>> outputStream = imageStream.map(pair->{
-           String name = pair.getLeft();
-           BufferedImage img = pair.getRight();
-           for(int i =0; i < img.getHeight(); i++){
-               for (int j = 0; j < img.getWidth(); j++) {
-                   int rgb = img.getRGB(i, j);
-                   Color color = new Color(rgb);
-                   int red = color.getRed();
-                   int blue = color.getBlue();
-                   int green = color.getGreen();
-                   Color outColor = new Color(red, blue, green);
-                   int outRgb = outColor.getRGB();
-                   img.setRGB(i, j, outRgb);
-               }
-           }
+        imageStream.forEach(pair ->{
+            String name = pair.getLeft();
+            BufferedImage img = pair.getRight();
+
+            for(int i =0; i < img.getWidth();i++ ){
+                for(int j =0; j < img.getHeight(); j++){
+                    int rgb = img.getRGB(i, j);
+                    Color color = new Color(rgb);
+                    int red = color.getRed();
+                    int blue = color.getBlue();
+                    int green = color.getGreen();
+                    Color outColor = new Color(red, blue, green);
+                    int outRgb = outColor.getRGB();
+                    img.setRGB(i,j,outRgb);
+                }
+            }
+
+            File output = new File("Output\\"+name);
             try {
-                ImageIO.write(img, "jpg", new File("Output\\" + name));
+                ImageIO.write(img, "jpg", output);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Pair<String, BufferedImage> out = Pair.of(name, img);
-            return out;
         });
 
-        List<Pair<String, BufferedImage>> list2 = outputStream.toList();
-        /*outputStream.forEach(pair->{
-            BufferedImage img = pair.getRight();
-            String name = pair.getLeft();
-            try {
-                ImageIO.write(img, "jpg", new File("Output\\" + name));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });*/
+        long timeElapsed = System.currentTimeMillis() - time;
 
-
+        System.out.println("Czas trwania: " + timeElapsed/1000 + " sekund");
 
 
 
